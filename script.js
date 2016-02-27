b2b209aecd18d463e41698a1d3012ad08a576a93
Chase Brasfield <chasebrasfield@gmail.com>
2016-02-27 10:15:11 -0600

    space bar is working properly.

--- script.js
+++ script.js
@@ -23,24 +23,38 @@ if (key.which == 16){
 })
 
 
+$(this).on('keydown',function(key){
+    if (key.which == 32){
+    $("#" + 32).css ({
+        "background-color":"green"
+    });
+    console.log(key);
+    
+    }
+})
+
+$(this).on('keyup',function(key){
+    if (key.which == 32){
+    $("#" + 32).css ({
+        "background-color":"#f5f5f5"
+    });
+    console.log(key);
+    
+    }
+})
 
 
 
 
 $(this).on('keydown', function(event) {
   var keyCode = event.which;
-  console.log(keyCode);
-  console.log(String.fromCharCode(keyCode));
   $("#" + String.fromCharCode(keyCode).toLowerCase()) .css ({
       "background-color":"green"
   });
-  console.log("#" + keyCode);
 })
 
 $(this).on('keyup',function(event) {
   var keyCode = event.which;
-  console.log(String.fromCharCode(keyCode).toLowerCase());
-  console.log(keyCode)
   $("#" + String.fromCharCode(keyCode).toLowerCase()) .css ({
       "background-color":"#f5f5f5"
   });
@@ -49,8 +63,6 @@ $(this).on('keyup',function(event) {
 
 $(this).on('keydown', function(event) {
   var keyCode = event.which;
-  console.log(keyCode);
-  console.log(String.fromCharCode(keyCode));
   $("#" + String.fromCharCode(keyCode)) .css ({
       "background-color":"green"
   });
@@ -59,10 +71,9 @@ $(this).on('keydown', function(event) {
 
 $(this).on('keyup',function(event) {
   var keyCode = event.which;
-  console.log(String.fromCharCode(keyCode).toLowerCase());
-  console.log(keyCode)
   $("#" + String.fromCharCode(keyCode)) .css ({
       "background-color":"#f5f5f5"
   });
+  
       
 })
