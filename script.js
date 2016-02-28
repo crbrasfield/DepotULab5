879e8a55548328bb32f5abf4c71aa4b52e5b7ba8
Chase Brasfield <chasebrasfield@gmail.com>
2016-02-27 19:42:11 -0600

    made my code more dry. Keyboard is working properly.

--- script.js
+++ script.js
@@ -1,79 +1,89 @@
-$( document ).ready(function() {
-    console.log(this);
+//hide upper case keyboard on doc ready
+$(document).ready(function () {
     console.log("ready!");
-    
-    $("#keyboard-upper-container").hide();    
-});
 
-
-
-
-$(this).on('keydown',function(key){
-if (key.which == 16){
-    $("#keyboard-upper-container").show();
-    $("#keyboard-lower-container").hide();
-}
-});
-
-$(this).on('keyup',function(key){
-if (key.which == 16){
-    $("#keyboard-lower-container").show();
     $("#keyboard-upper-container").hide();
-}
-})
+     
+});
 
+//shift keydown shows uppercase/hides lowercase
+$(this).on('keydown', function (key) {
+    if (key.which == 16) {
+        $("#keyboard-upper-container").show();
+        $("#keyboard-lower-container").hide();
+        $(".words").append('hello');
+    }
+});
 
-$(this).on('keydown',function(key){
-    if (key.which == 32){
-    $("#" + 32).css ({
-        "background-color":"green"
-    });
-    console.log(key);
-    
+//shift keyup shows lowercase/hides uppercase
+$(this).on('keyup', function (key) {
+    if (key.which == 16) {
+        $("#keyboard-lower-container").show();
+        $("#keyboard-upper-container").hide();
     }
 })
 
-$(this).on('keyup',function(key){
-    if (key.which == 32){
-    $("#" + 32).css ({
-        "background-color":"#f5f5f5"
-    });
-    console.log(key);
-    
+//spacebar keydown styles spacebar
+$(this).on('keydown', function (key) {
+    if (key.which == 32) {
+        $("#" + key.which).css({
+            "background-color": "green"
+        });
+        console.log(key);
+
+    }
+})
+
+//spacebar keyup styles spacebar to original color
+$(this).on('keyup', function (key) {
+    if (key.which == 32) {
+        $("#" + key.which).css({
+            "background-color": "#f5f5f5"
+        });
+        console.log(key);
+
     }
 })
 
 
 
 
-$(this).on('keydown', function(event) {
-  var keyCode = event.which;
-  $("#" + String.fromCharCode(keyCode).toLowerCase()) .css ({
-      "background-color":"green"
-  });
+$(this).on('keydown', function (event) {
+    var keyCode = event.which;
+    $("#" + String.fromCharCode(keyCode)).css({
+        "background-color": "green"
+    });
+    $("#" + (keyCode)).css({
+        "background-color": "green"
+    });
+    $("#" + String.fromCharCode(keyCode).toLowerCase()).css({
+        "background-color": "green"
+    });
+     $("#" + (keyCode)).css({
+        "background-color": "green"
+    });
+    $("#" + String.fromCharCode(keyCode).toLowerCase()).css({
+        "background-color": "green"
+    });
+    
 })
 
-$(this).on('keyup',function(event) {
-  var keyCode = event.which;
-  $("#" + String.fromCharCode(keyCode).toLowerCase()) .css ({
-      "background-color":"#f5f5f5"
-  });
-      
+
+$(this).on('keyup', function (event) {
+    var keyCode = event.which;
+    $("#" + String.fromCharCode(keyCode)).css({
+        "background-color": "#f5f5f5"
+    });
+     $("#" + (keyCode)).css({
+        "background-color": "#f5f5f5"
+    });
+    $("#" + String.fromCharCode(keyCode).toLowerCase()).css({
+        "background-color": "#f5f5f5"
+    });
+
+
 })
 
-$(this).on('keydown', function(event) {
-  var keyCode = event.which;
-  $("#" + String.fromCharCode(keyCode)) .css ({
-      "background-color":"green"
-  });
-  console.log("#" + keyCode);
-})
 
-$(this).on('keyup',function(event) {
-  var keyCode = event.which;
-  $("#" + String.fromCharCode(keyCode)) .css ({
-      "background-color":"#f5f5f5"
-  });
-  
-      
-})
+
+
